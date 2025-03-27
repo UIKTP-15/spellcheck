9a06e2dacf3c038d609e35d3b0176cd554c6c42f
thehidden1 <gorgi.siv@gmail.com>
2025-03-27 21:58:28 +0100

    Initial project

---
+++ spell-check-back-end/src/main/java/finki_ukim/spell_check_back_end/SpellCheckBackEndApplication.java
@@ -0,0 +1,13 @@
+package finki_ukim.spell_check_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellCheckBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpellCheckBackEndApplication.class, args);
+	}
+
+}
---
+++ spell-check-back-end/src/test/java/finki_ukim/spell_check_back_end/SpellCheckBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package finki_ukim.spell_check_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpellCheckBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
