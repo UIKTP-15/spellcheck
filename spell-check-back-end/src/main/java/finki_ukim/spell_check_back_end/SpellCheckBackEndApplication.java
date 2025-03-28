23da162ff86071448eadcb1de655b8b045d60209
= <bojan.vasilevski@students.finki.ukim.mk>
2025-03-28 19:13:27 +0100

    Added connection to db

--- spell-check-back-end/src/main/java/finki_ukim/spell_check_back_end/SpellCheckBackEndApplication.java
+++ spell-check-back-end/src/main/java/finki_ukim/spell_check_back_end/SpellCheckBackEndApplication.java
@@ -2,8 +2,10 @@ package finki_ukim.spell_check_back_end;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "file:.env", ignoreResourceNotFound = true)
 public class SpellCheckBackEndApplication {
 
 	public static void main(String[] args) {
